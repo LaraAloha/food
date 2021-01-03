639fbc6bb374ed082015ca731a2080e43f9017c0
Бобровникова Лариса Владимировна <ext.lbobrovnikova@tinkoff.ru>
2021-01-03 14:05:31 +0000

    src | added all rn project

---
+++ src/components/SearchBar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Foundation } from '@expo/vector-icons';
+import { TextInput } from 'react-native-gesture-handler';
+
+export const SearchBar = ({ term, onChangeTerm }) => {
+  return (
+    <View style={styles.background}>
+      <Foundation
+        name="search"
+        style={styles.searchIcon} />
+
+      <TextInput
+        placeholder="search"
+        style={styles.input}
+        value={term}
+        onChangeText={newTerm => onChangeTerm(newTerm)}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: 'gray',
+    borderRadius: 5,
+    height: 50,
+    marginHorizontal: 15,
+    flexDirection: 'row'
+
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    flex: 1,
+    fontSize: 16
+  },
+  searchIcon: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 15
+
+  }
+});
